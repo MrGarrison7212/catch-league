5e2c06e24d8bb99a2a5cd9b33da31e49b858c4a5
MrGarrison7212 <bokipfc1992@gmail.com>
2025-11-04 03:31:08 +0100

    add repository package + AnglerRepository

---
+++ backend/src/main/java/com/bojan/catchleague/backend/repository/AnglerRepository.java
@@ -0,0 +1,7 @@
+package com.bojan.catchleague.backend.repository;
+
+import com.bojan.catchleague.backend.model.Angler;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnglerRepository extends JpaRepository<Angler, Long> {
+}
