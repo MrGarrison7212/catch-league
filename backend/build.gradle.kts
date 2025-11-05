e7d725731272b5e8134e8edac4a218428453f392
MrGarrison7212 <bokipfc1992@gmail.com>
2025-11-05 03:37:40 +0100

    update Build_gradle - validation

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -24,6 +24,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
 	implementation ("org.apache.commons:commons-lang3:3.18.0")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
 	runtimeOnly("com.h2database:h2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
