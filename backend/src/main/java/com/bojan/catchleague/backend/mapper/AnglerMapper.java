6aaf2c46dc9ca89af5d7ed2d4aff6d0bb93b7ea9
MrGarrison7212 <bokipfc1992@gmail.com>
2025-11-07 02:18:25 +0100

    add - mapper package + AnglerMapper.

---
+++ backend/src/main/java/com/bojan/catchleague/backend/mapper/AnglerMapper.java
@@ -0,0 +1,12 @@
+package com.bojan.catchleague.backend.mapper;
+
+import com.bojan.catchleague.backend.dto.AnglerDto;
+import com.bojan.catchleague.backend.model.Angler;
+
+public final class AnglerMapper {
+    private AnglerMapper(){}
+
+    public static AnglerDto toDto(Angler a){
+        return new AnglerDto(a.getId(), a.getName());
+    }
+}
