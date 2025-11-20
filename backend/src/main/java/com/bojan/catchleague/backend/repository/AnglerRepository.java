c6b02509ab8b4c75e656197129b1d8483dcc338d
MrGarrison7212 <bokipfc1992@gmail.com>
2025-11-20 01:41:43 +0100

    update AnglerRepository - additional check

--- backend/src/main/java/com/bojan/catchleague/backend/repository/AnglerRepository.java
+++ backend/src/main/java/com/bojan/catchleague/backend/repository/AnglerRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AnglerRepository extends JpaRepository<Angler, Long> {
     boolean existsByEmail(String email);
+    boolean existsByEmailAndIdNot(String email, Long id);
 }
