023bfc11441ef221a32ceda30d3e6373879bd3c8
MrGarrison7212 <bokipfc1992@gmail.com>
2025-11-26 01:48:55 +0100

    add - FishingLocation - add fields

--- backend/src/main/java/com/bojan/catchleague/backend/model/FishingLocation.java
+++ backend/src/main/java/com/bojan/catchleague/backend/model/FishingLocation.java
@@ -1,4 +1,29 @@
 package com.bojan.catchleague.backend.model;
 
+import jakarta.persistence.*;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "fishing_location")
 public class FishingLocation {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Type type;
+
+    @Column(nullable = false, updatable = false)
+    private Instant createdAt;
+
+    @Column(nullable = false)
+    private Instant updatedAt;
+
+    public enum Type { RIVER, LAKE, POND, STREAM, CANAL }
+
 }
